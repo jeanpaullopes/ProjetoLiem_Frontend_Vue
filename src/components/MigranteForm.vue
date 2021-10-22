ed30a9f14059ef2236a3910c6d4b2f9922871c2a
Prof Jean Paul <jean.paul@uniritter.edu.br>
2021-10-22 19:05:15 -0300

    aqui op profe caiu

---
+++ src/components/MigranteForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <form action="#">
+    {{dado}}
+    {{dado.nome}}
+      nome <input v-model="dado.nome"/>
+ <!-- 
+      <p>email: {{migranteData.email}}</p>    
+      <p>telefone: {{migranteData.telefone}}</p> 
+     
+      idiomas: <button type="button" @click="addIdioma">+</button><br/>
+      <div v-for="idioma in migranteData.idiomas" :key="idioma">
+         <input :value='idioma'/>
+      </div>
+      -->
+
+  </form>
+</template>
+
+<script>
+export default {
+    name: "MigranteForm",
+    data(){ return {
+         dado: Object
+    }
+    },
+    props: {
+        migrante: Object
+    },
+    created() {
+      console.log(this.migrante.nome)
+      console.log(this.dado.nome)
+      this.dado = this.migrante
+      console.log(this.dado.nome)
+      
+    }
+    //,
+    //methods:{
+    //  addIdioma(){
+    //    console.log(this.migranteData)
+    //    console.log(this.migranteData.idiomas)
+    //    this.migranteData.idiomas.push("")
+    //  }
+    //}
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <migrante-detalhe :migrante="objMigrante"/>
+    {{objMigrante.nome}}
+    <migrante-form :migrante="objMigrante"/>
+    <hr/>
+   <!-- <migrante-detalhe :migrante="objMigrante"/>
+   -->
   </div>
 </template>
 <script>
-import MigranteDetalhe from '../components/MigranteDetalhe.vue'
+//import MigranteDetalhe from '../components/MigranteDetalhe.vue'
+import MigranteForm from '../components/MigranteForm.vue'
 import STORE from '../store/index'
 export default {
-  components: { MigranteDetalhe },
+  components: { /*MigranteDetalhe,*/ MigranteForm },
   name: 'About',
   data() {
     return {
       objMigrante: Object
     }
   },
-  created(){
-    (STORE.getters.getMigrante).then((response)=>{console.log(response); this.objMigrante = response})
+  async created(){
+    this.objMigrante = await STORE.getters.getMigrante;
+    console.log(this.objMigrante)
   }
 }
-</script>ipt
+</script>
